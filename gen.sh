74a53e281c145fda4599aa6eca6c8d54e12ba16e
ThatGeekyWeeb <thatgeekyweeb@gmail.com>
2021-06-12 22:32:32 -0400

    edit to gen.sh to expect sed's existance; and to use a pure sh cat, also remove tr usage using read loop

--- gen.sh
+++ gen.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 gitd=$PWD
-# Ash breaks with `cd <dir> || exit` So alot of `if` checks happen
 has() {
-        case "$(command -v "$1" 2>/dev/null)" in
-                alias*|"") return 1
-        esac
+  case "$(command -v "$1" 2>/dev/null)" in
+    alias*|"") return 1
+  esac
 }
-cd "${gitd}"
-if [ ! -d ./blobmoji/png ]; then if has git; then git submodule update --init; else echo "FAIL, git missing, please find your own way to download the submods"; exit 1;fi;fi
-# git isn't ""REALLY"" needed, you just have to trick the script into finding the submods
-if ! has sed; then echo "sed NOT FOUND!!"; exit 1; fi
+shcat(){
+  out="$(while read -r p; do
+    IFS=""; echo "$p"
+  done <&0)"
+  IFS=""; echo "$out"
+}
 grep "//" mapping.go | sed -e 's/:.*//g' -e 's/[[:blank:]]//g' -e 's/"//g' >> temp1
-cat mapping.go | sed -e '/\/\//d' -e '/u/,$!d' -e 's/"//g' -e 's/:[[:space:]]/=/g' -e 's/\\//g' -e 's/,//g' -e 's/\t//g' -e 's/}//g' -e '/^$/d' > temp2
+shcat < mapping.go | sed -e '/\/\//d' -e '/u/,$!d' -e 's/"//g' -e 's/:[[:space:]]/=/g' -e 's/\\//g' -e 's/,//g' -e 's/\t//g' -e 's/}//g' -e '/^$/d' > temp2
 while IFS='=' read -r name val; do # Thx Perish :)
 	IFS=''
-	printf 'img[alt=":%s:"],li[aria-label^=":%s:"]>div{content:var(--a);background-image:var(--a)!important;--a:url("data:image/png;base64,%s");background-size:contain;object-position: var(--op);--op:-9999px -9999px;}\n' "${name}" "${name}" "$(base64 blobmoji/png/128/emoji_${val}.png | tr -d '\n')"
+  printf 'img[alt=":%s:"],li[aria-label^=":%s:"]>div{content:var(--a);background-image:var(--a)!important;--a:url("data:image/png;base64,%s");background-size:contain;object-position: var(--op);--op:-9999px -9999px;}\n' "${name}" "${name}" "$(base64 blobmoji/png/128/emoji_${val}.png | while read -r p; do IFS=""; printf "$p"; done)"
 done < ./temp2
-rm ./missing
-while read -r i; do
-	echo "$i CANNOT BE MATCHED" >> ./missing
-done < temp1
 rm temp1 temp2
